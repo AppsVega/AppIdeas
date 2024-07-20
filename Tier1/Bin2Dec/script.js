a5a2e8828ecb65ed552c161a84c5f412b8ed2d49
Mateus Vega <154934279+MateusVega@users.noreply.github.com>
2024-07-20 12:44:33 +0000

    add bin2dec project

---
+++ Tier1/Bin2Dec/script.js
@@ -0,0 +1,13 @@
+function calculate() {
+  let num = document.getElementById("bin").value;
+  let array_num = num.split("");
+  array_num = array_num.reverse();
+  let array_length = array_num.length;
+  dec = 0;
+  for (let cont = 0; cont < array_length; cont++) {
+    dec += 2 ** cont * array_num[cont];
+  }
+  document.getElementById("result").innerHTML = `Result: ${dec}`;
+}
+
+calculate();
